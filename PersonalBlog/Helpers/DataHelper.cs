740426a779de8e68b7e8a35ec30ed6a2c2294790
Brock <Lynch.b@hotmail.com>
2022-09-23 12:49:07 -0400

    helpers

---
+++ PersonalBlog/Helpers/DataHelper.cs
@@ -0,0 +1,13 @@
+﻿using PersonalBlog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PersonalBlog.Helpers
+{
+    public class DataHelper
+    {public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
