24a9f306908ddfd2823ee5be10d1b3c393a1c0c8
Brock <Lynch.b@hotmail.com>
2023-09-01 10:42:12 -0400

    fixed non admin access to admin pages

--- Controllers/TagsController.cs
+++ Controllers/TagsController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using PersonalBlog.Models;
 
 namespace PersonalBlog.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class TagsController : Controller
     {
         private readonly ApplicationDbContext _context;
